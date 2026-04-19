88deaf0d068684d4947ab683edbf8644d457951e
Geosistemas <administracion@geo-sistemas.com.ar>
2026-04-19 15:22:47 -0300

    Add files via upload

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/panel/sw.js', { scope: '/panel/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
